a61d25f90fde24f1189f7024cf6416ce44298627
Simeon Nedelchev <simonnedelchev@gmail.com>
2024-01-11 13:53:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const { token } = require('./config/config.json');
 
 //  New client instance
 
