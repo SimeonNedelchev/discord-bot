522c136652fac4f065f552dee1719cbc41525678
Simeon Nedelchev <simonnedelchev@gmail.com>
2024-03-01 08:03:43 +0200

    added "daily forecast" and "current conditions" commands

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { AttachmentBuilder, Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config/config.json');
 
 //  New client instance
